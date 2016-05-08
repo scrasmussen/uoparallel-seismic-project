efd3888fdc4b02cdc3ee52a53483f3e1f00dfa8e
Atlee Brink <abrink@uoregon.edu>
2016-05-07 18:27:30 -0700

    changed example to use expected file

--- filers/example_vbox.c
+++ filers/example_vbox.c
@@ -2,7 +2,7 @@
 #include <stdio.h> /* for stdout, not necessary otherwise */
 int main() {
     struct VELOCITYBOX vbox;
-    if( !vboxloadtext( &vbox, "velocities.txt" ) ) { /* handle error */ }
+    if( !vboxloadtext( &vbox, "velocity-241-241-51.txt" ) ) { /* handle error */ }
     if( !vboxstorebinary( "velocities.vbox", vbox ) ) { /* handle error */ }
     vboxfree( &vbox );
     if( !vboxloadbinary( &vbox, "velocities.vbox" ) ) { /* handle error */ }
